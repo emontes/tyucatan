538ce06d9a634b747d0aa969ba1094fa958477c8
emontes <emontes@turista.com.mx>
2020-08-01 00:17:05 -0500

    se pone en blanco instagram.js para evitar llamadas a graphql

--- src/components/Banner/Instagram.js
+++ src/components/Banner/Instagram.js
@@ -1,54 +1,11 @@
-import React from "react"
-import Title from "./Title"
-import Image from "gatsby-image"
-import styled from "styled-components"
-import { graphql, useStaticQuery } from "gatsby"
+import React from 'react'
 
-const query = graphql`
-  {
-    allInstaNode(limit: 6) {
-      nodes {
-        localFile {
-          childImageSharp {
-            fluid {
-              ...GatsbyImageSharpFluid
-            }
-          }
-        }
-      }
-    }
-  }
-`
 const Instagram = () => {
-  const data = useStaticQuery(query)
-
-  const {
-    allInstaNode: { nodes },
-  } = data
-
   return (
-    <Wrapper>
-      <Title title="instagram"></Title>
-      <div className="images">
-        {nodes.map((item, index) => {
-          const {
-            localFile: {
-              childImageSharp: { fluid },
-            },
-          } = item
-          return <Image fluid={fluid} key={index} />
-        })}
-      </div>
-    </Wrapper>
+    <div>
+      Hello from instagram, checa el codigo en los commits anteriores
+    </div>
   )
 }
-
-const Wrapper = styled.article`
-  .images {
-    display: grid;
-    grid-template-columns: 1fr 1fr 1fr;
-    gap: 1rem;
-  }
-`
 
 export default Instagram
--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -11,7 +11,7 @@ const Banner = () => {
     <Wrapper>
       <About />
       <HotelsBox />
-      {/* <Instagram /> */}
+      
       <Recent />
       <BannerCategories />
     </Wrapper>
